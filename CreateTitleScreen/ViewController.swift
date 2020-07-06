1221c172c87897f61521da2f5b0f0932eb06e3c2
Eric Park <safetotry2@gmail.com>
2020-07-06 12:13:18 -0400

    Initial Commit

---
+++ CreateTitleScreen/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CreateTitleScreen
+//
+//  Created by Mac on 7/6/20.
+//  Copyright © 2020 Eric Park. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
